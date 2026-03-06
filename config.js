4e2c4cccdbdc0425a3260a61abf4256a5ced53d3
ADMON5087 <taemini3355@gmail.com>
2026-03-06 21:47:36 +0900

    Add files via upload

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
-// ✅ Supabase 설정 - 본인 프로젝트 값으로 교체하세요
+// ✅ Supabase 설정
 const APP_CONFIG = {
   SUPABASE_URL: "https://gatxogfdfsyoyqeirioj.supabase.co",
   SUPABASE_ANON_KEY: "sb_publishable_Z5bkS3n79EwK4AdlFWpw4Q_DT50ANwr"
